3930ce5e8a1f341c27fe268de704d51d5e27e5f3
Deavelleye <Deavelleye@yahoo.com>
2017-03-13 13:53:25 +0100

    Initial commit 0.3

---
+++ src/cerberus_ac/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+
+class UserGroup(models.Model):
+
+class Role(models.Model):
+
+class Action()
--- src/cerberus_ac/urls.py
+++ src/cerberus_ac/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'')
+    url(r'^permissions/edit/$', views.editperm, name='edit_permissions'),
+    url(r'^permissions/view/$', views.viewperm, name='view_permissions'),
+    url(r'^member_info/$', views.memberinfo, name='view_member_info'),
+    url(r'^logs/object_access/$', views.logs_access, name='logs_access'),
+    url(r'^logs/permission_changes/$', views.perm_changes, name='perm_changes'),
 ]
