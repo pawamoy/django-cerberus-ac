8a039243a02746777bcb8899aa506b2ea004347f
Deavelleye <Deavelleye@yahoo.com>
2017-03-24 16:17:47 +0100

    Initial commit 0.4
    Adding templates and some static files

--- src/cerberus_ac/models.py
+++ src/cerberus_ac/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class User(models.Model):
+    pass
 
 class UserGroup(models.Model):
+    pass
 
 class Role(models.Model):
+    pass
 
-class Action()
+class Action():
+    pass
--- src/cerberus_ac/urls.py
+++ src/cerberus_ac/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^permissions/edit/$', views.editperm, name='edit_permissions'),
-    url(r'^permissions/view/$', views.viewperm, name='view_permissions'),
+    url(r'^permissions/group/edit/$', views.editgroupperm, name='edit_group_permissions'),
+    url(r'^permissions/group/view/$', views.viewgroupperm, name='view_group_permissions'),
+    url(r'^permissions/user/view/$', views.edituserperm, name='edit_user_permissions'),
+    url(r'^permissions/user/view/$', views.viewuserperm, name='view_user_permissions'),
     url(r'^member_info/$', views.memberinfo, name='view_member_info'),
     url(r'^logs/object_access/$', views.logs_access, name='logs_access'),
     url(r'^logs/permission_changes/$', views.perm_changes, name='perm_changes'),
