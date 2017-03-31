188812fe67a6b1351f6da1f4ac362b0db052406e
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-03-31 16:03:03 +0200

    Write views and templates

--- src/cerberus_ac/urls.py
+++ src/cerberus_ac/urls.py
@@ -7,40 +7,64 @@ from django.conf.urls import url
 from . import views
 
 
-def cerberus_urlpatterns(admin_view):
+def cerberus_urlpatterns(admin_view_func=lambda x: x):
+    """
+    Function returning URLs, optionally wrapped into an admin_view function.
+
+    Args:
+        admin_view_func (func):
+            the ``admin_view`` function of an ``AdminSite``. Will default to
+            identity.
+
+    Returns:
+        list: list of URLs patterns.
+    """
     return [
-        # url(r'^$', admin_view(views.index), name='index'),
+        url(r'^$', admin_view_func(views.Index.as_view()),
+            name='cerberus_index'),
+
+        url(r'^permissions/$',
+            admin_view_func(views.Permissions.as_view()),
+            name='permissions'),
 
         # user views
-        # url(r'^permissions/user/view/$',
-        #     admin_view(views.ViewUserPerm.as_view()),
-        #     name='view_user_permissions'),
+        url(r'^permissions/user/$',
+            admin_view_func(views.UserPermissions.as_view()),
+            name='user_permissions'),
+        url(r'^permissions/user/view/$',
+            admin_view_func(views.ViewUserPermissions.as_view()),
+            name='view_user_permissions'),
         url(r'^permissions/user/edit/$',
-            admin_view(views.EditUserPerm.as_view()),
+            admin_view_func(views.EditUserPermissions.as_view()),
             name='edit_user_permissions'),
         url(r'^permissions/user/edit/(?P<user>\d+)/$',
-            admin_view(views.edit_user_perm_post),
+            admin_view_func(views.edit_user_perm_post),
             name='edit_user_permissions_post'),
 
         # group views
-        # url(r'^permissions/group/view/$',
-        #     admin_view(views.ViewGroupPerm.as_view()),
-        #     name='view_group_permissions'),
+        url(r'^permissions/group/$',
+            admin_view_func(views.GroupPermissions.as_view()),
+            name='group_permissions'),
+        url(r'^permissions/group/view/$',
+            admin_view_func(views.ViewGroupPermissions.as_view()),
+            name='view_group_permissions'),
         url(r'^permissions/group/edit/$',
-            admin_view(views.EditGroupPerm.as_view()),
+            admin_view_func(views.EditGroupPermissions.as_view()),
             name='edit_group_permissions'),
         url(r'^permissions/group/edit/(?P<group>\d+)/$',
-            admin_view(views.edit_group_perm_post),
+            admin_view_func(views.edit_group_perm_post),
             name='edit_group_permissions_post'),
 
         # other views
         # url(r'^member_info/$',
-        #     admin_view(views.MemberInfo.as_view()),
+        #     admin_view_func(views.MemberInfo.as_view()),
         #     name='view_member_info'),
         # url(r'^logs/object_access/$',
-        #     admin_view(views.ObjectAccess.as_view()),
+        #     admin_view_func(views.ObjectAccess.as_view()),
         #     name='logs_access'),
         # url(r'^logs/permission_changes/$',
-        #     admin_view(views.PermChanges.as_view()),
+        #     admin_view_func(views.PermChanges.as_view()),
         #     name='perm_changes'),
     ]
+
+urlpatterns = cerberus_urlpatterns()
--- src/cerberus_ac/views.py
+++ src/cerberus_ac/views.py
@@ -12,21 +12,45 @@ resources_list = AppSettings.get_actual_resources_classes()
 role_classes = AppSettings.get_actual_roles_classes()
 
 
-def edit_user_perm_post(request, user):
-    """Handler for user permissions POSTs."""
-    if request.method == "POST":
-        pass
+class Index(DashboardView):
+    """Cerberus menu."""
+
+    title = _('Index - Cerberus AC')
+    crumbs = ({'name': _('Cerberus'), 'url': 'admin:cerberus_index'}, )
+    grid = Grid(Row(Column(Box(
+        title='Cerberus Access Control',
+        template='cerberus_ac/index.html'))))
 
 
-class EditUserPerm(DashboardView):
-    """Dashboard view to see user permissions."""
-    title = _('Edit User Permissions')
-    crumbs = (
-        {'name': _('Cerberus')},
-        {'name': _('Permissions')},
-        {'name': _('Users')},
-        {'name': _('Edit'), 'url': 'admin:edit_user_perm'}
-    )
+class Permissions(Index):
+    """Menu for permissions."""
+
+    title = _('Permissions - Cerberus AC')
+    crumbs = ({'name': _('Permissions'), 'url': 'admin:permissions'}, )
+    grid = Grid(Row(Column(Box(template='cerberus_ac/permissions.html'))))
+
+
+class UserPermissions(Permissions):
+    """Menu for user permissions."""
+
+    title = _('User Permissions - Cerberus AC')
+    crumbs = ({'name': _('User'), 'url': 'admin:user_permissions'}, )
+    grid = Grid(Row(Column(Box(template='cerberus_ac/user_permissions.html'))))
+
+
+class ViewUserPermissions(UserPermissions):
+    """View to see user permissions."""
+
+    title = _('View User Permissions - Cerberus AC')
+    crumbs = ({'name': _('View'), 'url': 'admin:view_user_permissions'}, )
+    grid = Grid(Row(Column(Box(template='cerberus_ac/view_user_permissions.html'))))  # noqa
+
+
+class EditUserPermissions(UserPermissions):
+    """View to edit user permissions."""
+
+    title = _('Edit User Permissions - Cerberus AC')
+    crumbs = ({'name': _('Edit'), 'url': 'admin:edit_user_permissions'}, )
 
     role_instances = []
     for r in role_classes:
@@ -37,9 +61,52 @@ class EditUserPerm(DashboardView):
         resources_real_list.extend(res.objects.all())
 
     grid = Grid(Row(Column(
-        Box(template='cerberus_ac/edit_user_perms.html',
+        Box(template='cerberus_ac/edit_user_permissions.html',
             context={'members': role_instances[:10:],
                      'resources': resources_real_list[:10:]})
+    )))
+
+
+def edit_user_perm_post(request, user):
+    """Handler for user permissions POSTs."""
+    if request.method == "POST":
+        pass
+
+
+class GroupPermissions(Permissions):
+    """Menu for group permissions."""
+
+    title = _('Group Permissions - Cerberus AC')
+    crumbs = ({'name': _('Group'), 'url': 'admin:group_permissions'}, )
+    grid = Grid(Row(Column(Box(template='cerberus_ac/group_permissions.html'))))  # noqa
+
+
+class ViewGroupPermissions(GroupPermissions):
+    """View to see user permissions."""
+
+    title = _('View Group Permissions - Cerberus AC')
+    crumbs = ({'name': _('View'), 'url': 'admin:view_group_permissions'}, )
+    grid = Grid(Row(Column(Box(template='cerberus_ac/view_group_permissions.html'))))  # noqa
+
+
+class EditGroupPermissions(GroupPermissions):
+    """View to edit group permissions."""
+
+    title = _('Edit Group Permissions - Cerberus AC')
+    crumbs = ({'name': _('Edit'), 'url': 'admin:edit_group_permissions'}, )
+
+    role_instances = []
+    for r in role_classes:
+        role_instances.extend(r.objects.all())
+
+    resources_real_list = []
+    for res in resources_list:
+        resources_real_list.extend(res.objects.all())
+
+    grid = Grid(Row(Column(
+        Box(template='cerberus_ac/edit_group_permissionss.html',
+            context={'members': role_instances,
+                     'resources': resources_real_list})
     )))
 
 
@@ -47,21 +114,3 @@ def edit_group_perm_post(request, user):
     """Handler for group permissions POSTs."""
     if request.method == "POST":
         pass
-
-
-class EditGroupPerm(DashboardView):
-    """Dashboard view to see group permissions."""
-
-    role_instances = []
-    for r in role_classes:
-        role_instances.extend(r.objects.all())
-
-    resources_real_list = []
-    for res in resources_list:
-        resources_real_list.extend(res.objects.all())
-
-    grid = Grid(Row(Column(
-        Box(template='cerberus_ac/edit_user_perms.html',
-            context={'members': role_instances,
-                     'resources': resources_real_list})
-    )))
