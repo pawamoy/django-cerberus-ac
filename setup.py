806515e8712e7f7c19a468a52f2fa96d1a19dc00
Deavelleye <Deavelleye@yahoo.com>
2017-04-03 15:41:10 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-cerberus-ac',
-    version='0.1.1',
+    version='0.1.2',
     license='ISC',
     description='Django Ontology Based Access Control Module',
     long_description='%s\n%s' % (
--- src/cerberus_ac/__init__.py
+++ src/cerberus_ac/__init__.py
@@ -5,7 +5,7 @@ r"""Cerberus Access Control package."""
 import importlib
 from django.conf import settings
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 
 def _import(complete_path):
