6673ed29442a63882aeed29ea1217e6f04820887
Deavelleye <Deavelleye@yahoo.com>
2017-04-04 16:22:44 +0200

    Update 0.91

--- src/cerberus_ac/admin.py
+++ src/cerberus_ac/admin.py
@@ -2,9 +2,6 @@
 
 """Admin module."""
 
-# from django.contrib import admin
-# from django.contrib.admin.sites import AdminSite
-#
 # from cerberus_ac.views import EditUserPermissions
 # from .models import *
 #
@@ -21,38 +18,6 @@
 # security_admin_site = SecurityAdmin(name='SecurityAdmin')
 # data_admin_site = DataAdmin(name='DataAdmin')
 # audit_admin_site = AuditAdmin(name='AuditAdmin')
-#
-# # Security Admin Pages
-#     # Logs
-# @security_admin_site.register(AccessHistory)
-# class ObjectAccessHistoryAdmin(admin.ModelAdmin):
-#     pass
-#
-# @security_admin_site.register(PrivilegeHistory)
-# class PrivChangesHistoryAdmin(admin.ModelAdmin):
-#     pass
-#
-#     # User Permissions
-# @security_admin_site.register(RolePrivilege)
-# class PermissionsAdmin(admin.ModelAdmin):
-#     pass
-
-from cerberus_ac.views import EditUserPermissions
-from .models import *
-
-
-class SecurityAdmin(AdminSite):
-    pass
-
-class DataAdmin(AdminSite):
-    pass
-
-class AuditAdmin(AdminSite):
-    pass
-
-security_admin_site = SecurityAdmin(name='SecurityAdmin')
-data_admin_site = DataAdmin(name='DataAdmin')
-audit_admin_site = AuditAdmin(name='AuditAdmin')
 
 # # Security Admin Pages
 #     # Logs
