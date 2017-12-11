c1975e180a830a43827dea745c52b6493f7e9ae6
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-12-11 19:38:40 +0100

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-cerberus-ac',
-    version='0.2.2',
+    version='0.2.3',
     license='ISC',
     description='Django Ontology Based Access Control Module',
     long_description='%s\n%s' % (
